8cb3f1baf0e7b8655f00943727faf38d6ffba9b0
Tong Li <litong01@us.ibm.com>
2014-12-20 14:06:17 -0500

    implemented GET /v2.0/metrics

--- monasca/tests/v2/elasticsearch/test_metrics.py
+++ monasca/tests/v2/elasticsearch/test_metrics.py
@@ -12,13 +12,20 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import copy
 import falcon
 import mock
 from oslo.config import fixture as fixture_config
 from oslotest import base
+import requests
 
 from monasca.common import kafka_conn
 from monasca.v2.elasticsearch import metrics
+
+try:
+    import ujson as json
+except ImportError:
+    import json
 
 
 class TestMetricDispatcher(base.BaseTestCase):
@@ -29,7 +36,20 @@ class TestMetricDispatcher(base.BaseTestCase):
         self.CONF.metrics.topic = 'fake'
         self.CONF.es.uri = 'fake_es_uri'
         super(TestMetricDispatcher, self).setUp()
-        self.dispatcher = metrics.MetricDispatcher({})
+        res = mock.Mock()
+        res.status_code = 200
+        res.json.return_value = {"data": {"mappings": {"fake": {
+            "properties": {
+                "dimensions": {"properties": {
+                    "key1": {"type": "long"}, "key2": {"type": "long"},
+                    "rkey0": {"type": "long"}, "rkey1": {"type": "long"},
+                    "rkey2": {"type": "long"}, "rkey3": {"type": "long"}}},
+                "name": {"type": "string", "index": "not_analyzed"},
+                "timestamp": {"type": "string", "index": "not_analyzed"},
+                "value": {"type": "double"}}}}}}
+        with mock.patch.object(requests, 'get',
+                               return_value=res):
+            self.dispatcher = metrics.MetricDispatcher({})
 
     def test_connections(self):
         # test that the kafka connection uri should be 'fake' as it was passed
@@ -43,6 +63,10 @@ class TestMetricDispatcher(base.BaseTestCase):
         self.assertEqual(self.dispatcher._es_conn.doc_type, 'fake')
 
         self.assertEqual(self.dispatcher._es_conn.uri, 'fake_es_uri/')
+
+        # test that the query url is correctly formed
+        self.assertEqual(self.dispatcher._query_url,
+                         'fake_es_uri/monasca_*/fake/_search')
 
     def test_post_data(self):
         with mock.patch.object(kafka_conn.KafkaConnection, 'send_messages',
@@ -61,12 +85,71 @@ class TestMetricDispatcher(base.BaseTestCase):
         # test that the response code is 204
         self.assertEqual(getattr(falcon, 'HTTP_400'), res.status)
 
+    def test__handle_req_name(self):
+        req = mock.Mock()
+
+        def _side_effect(arg):
+            if arg == 'name':
+                return 'tongli'
+            elif arg == 'dimensions':
+                return 'key1:100, key2:200'
+        req.get_param.side_effect = _side_effect
+
+        body = copy.deepcopy(self.dispatcher._query_body)
+        self.dispatcher._handle_req_name(req, body)
+
+        self.assertEqual(body['query']['filtered']['filter']['bool']['must'],
+                         [{'term': {'name': 'tongli'}}])
+
+    def test__handle_req_dimensions(self):
+        req = mock.Mock()
+
+        def _side_effect(arg):
+            if arg == 'name':
+                return 'tongli'
+            elif arg == 'dimensions':
+                return 'key1:100, key2:200, key3:, key4, key24:12:29,'
+        req.get_param.side_effect = _side_effect
+
+        body = copy.deepcopy(self.dispatcher._query_body)
+        self.dispatcher._handle_req_dimensions(req, body)
+
+        # notice that the query parameter for dimension contains 3 invalid
+        # ones, these should be ignored.
+        self.assertEqual(body['query']['filtered']['filter']['bool']['must'],
+                         [{'term': {'dimensions.key1': '100'}},
+                          {'term': {'dimensions.key2': '200'}}])
+
     def test_do_get_metrics(self):
         res = mock.Mock()
-        self.dispatcher.do_get_metrics(mock.Mock(), res)
+        req = mock.Mock()
 
-        # test that the response code is 501
-        self.assertEqual(getattr(falcon, 'HTTP_501'), res.status)
+        def _side_effect(arg):
+            if arg == 'name':
+                return 'tongli'
+            elif arg == 'dimensions':
+                return 'key1:100, key2:200'
+        req.get_param.side_effect = _side_effect
+
+        req_result = mock.Mock()
+        req_result.json.return_value = {"aggregations": {
+            "name": {"doc_count_error_upper_bound": 10,
+                     "sum_other_doc_count": 903,
+                     "buckets": [{"key": "BJAMCA", "doc_count": 10,
+                                  "key1": {"doc_count_error_upper_bound": 0,
+                                           "sum_other_doc_count": 0,
+                                           "buckets": []}}]}}}
+
+        req_result.status_code = 200
+
+        with mock.patch.object(requests, 'post', return_value=req_result):
+            self.dispatcher.do_get_metrics(req, res)
+
+        # test that the response code is 200
+        self.assertEqual(res.status, getattr(falcon, 'HTTP_200'))
+        obj = json.loads(res.stream)
+        self.assertEqual(obj[0]['name'], 'BJAMCA')
+        self.assertEqual(obj[0]['dimensions'], {})
 
     def test_do_post_metrics(self):
         with mock.patch.object(kafka_conn.KafkaConnection, 'send_messages',
--- monasca/v2/elasticsearch/metrics.py
+++ monasca/v2/elasticsearch/metrics.py
@@ -14,14 +14,22 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import copy
 import falcon
 from oslo.config import cfg
+import requests
 
 from monasca.api import monasca_api_v2
 from monasca.common import es_conn
 from monasca.common import kafka_conn
 from monasca.common import resource_api
 from monasca.openstack.common import log
+
+try:
+    import ujson as json
+except ImportError:
+    import json
+
 
 metrics_opts = [
     cfg.StrOpt('topic', default='metrics',
@@ -43,6 +51,52 @@ class MetricDispatcher(monasca_api_v2.V2API):
             cfg.CONF.metrics.topic)
         self._es_conn = es_conn.ESConnection(
             cfg.CONF.metrics.topic)
+        # Setup the get metrics query body pattern
+        self._query_body = {
+            "query": {"filtered": {"filter": {"bool": {"must": []}}}},
+            "size": 0}
+
+        # Setup the get metrics query url, the url should be similar to this:
+        # http://host:port/data_20141201/metrics/_search
+        # the url should be made of es_conn uri, the index prefix, metrics
+        # dispatcher topic, then add the key word _search.
+        self._query_url = ''.join([self._es_conn.uri,
+                                  self._es_conn.index_prefix, '*/',
+                                  cfg.CONF.metrics.topic,
+                                  '/_search'])
+
+        # the url to get all the properties of metrics
+        self._query_mapping_url = ''.join([self._es_conn.uri,
+                                           self._es_conn.index_prefix,
+                                           '*/_mappings/',
+                                           cfg.CONF.metrics.topic])
+
+        self._dim_props = {}
+        self._get_dimension_keys()
+        self._make_agg_clause()
+
+    def _get_dimension_keys(self):
+        res = requests.get(self._query_mapping_url)
+        if res.status_code == 200:
+            for key in res.json():
+                mappings = res.json()[key]['mappings']
+                break
+            if mappings:
+                properties = mappings[cfg.CONF.metrics.topic]['properties']
+                if properties['dimensions']:
+                    self._dim_props = properties['dimensions']['properties']
+
+    def _make_agg_clause(self):
+        if self._dim_props:
+            self._query_body['aggs'] = {
+                'name': {'terms': {'field': 'name'},
+                         'aggs': {}}}
+            aggs = self._query_body['aggs']['name']['aggs']
+            for prop in self._dim_props:
+                aggs[prop] = {'terms': {'field': 'dimensions.' + prop,
+                                        'size': 0}}
+                aggs[prop]['aggs'] = {}
+                aggs = aggs[prop]['aggs']
 
     def post_data(self, req, res):
         LOG.debug('Getting the call.')
@@ -51,10 +105,80 @@ class MetricDispatcher(monasca_api_v2.V2API):
         code = self._kafka_conn.send_messages(msg)
         res.status = getattr(falcon, 'HTTP_' + str(code))
 
+    def _handle_req_name(self, req, body):
+        name = req.get_param('name')
+        if name:
+            body['query']['filtered']['filter']['bool']['must'].append(
+                {'term': {'name': name.strip()}})
+
+    def _handle_req_dimensions(self, req, body):
+        dimensions = req.get_param('dimensions')
+        if dimensions:
+            terms = []
+
+            def _handle_pair(pair):
+                param = pair.split(':')
+                if len(param) == 2 and param[0] and param[1]:
+                    key = param[0].strip()
+                    value = param[1].strip()
+                    try:
+                        value = float(param[1].strip)
+                    except Exception:
+                        pass
+                    terms.append({'term': {'dimensions.' + key: value}})
+            map(_handle_pair, dimensions.split(','))
+            body['query']['filtered']['filter']['bool']['must'] += terms
+
     @resource_api.Restify('/v2.0/metrics/', method='get')
     def do_get_metrics(self, req, res):
-        LOG.debug('Got the request!')
-        res.status = getattr(falcon, 'HTTP_501')
+        LOG.debug('The metrics GET request is received!')
+        body = copy.deepcopy(self._query_body)
+        self._handle_req_name(req, body)
+        self._handle_req_dimensions(req, body)
+
+        # if there is no name or dimension, we do not need filter clause
+        if not body['query']['filtered']['filter']['bool']['must']:
+            del body['query']
+
+        es_res = requests.post(self._query_url, data=json.dumps(body))
+        res.status = getattr(falcon, 'HTTP_%s' % es_res.status_code)
+
+        LOG.debug('Query to ElasticSearch returned: %s' % es_res.status_code)
+        if es_res.status_code == 200:
+            # convert the response into monasca metrics format
+            aggs = es_res.json()['aggregations']
+
+            flag = {'is_first': True}
+
+            def _fixup_obj(obj):
+                target = {'name': obj['name']}
+                del obj['name']
+                target['dimensions'] = obj
+                if flag['is_first']:
+                    flag['is_first'] = False
+                    return json.dumps(target)
+                else:
+                    return ',' + json.dumps(target)
+
+            def _make_body(obj, parent):
+                for key in parent:
+                    if key in ['key', 'doc_count']:
+                        continue
+                    if parent[key]['buckets']:
+                        for bucket in parent[key]['buckets']:
+                            new_obj = copy.deepcopy(obj)
+                            new_obj[key] = bucket['key']
+                            if bucket:
+                                yield ''.join(_make_body(new_obj, bucket))
+                            else:
+                                yield _fixup_obj(new_obj)
+                    else:
+                        yield _fixup_obj(obj)
+
+            res.stream = '[' + ''.join(_make_body({}, aggs)) + ']'
+            res.content_type = 'application/json'
+        else:
+            res.stream = ''
 
     @resource_api.Restify('/v2.0/metrics/', method='post')
     def do_post_metrics(self, req, res):
